09902d3429b1b1eadae2dae44ff05357cbf50393
nh <nh@nhMac14.local>
2023-02-05 18:27:14 +0900

    add 1b and .gitignore

--- ITP1/1a.go
+++ ITP1/1a.go
@@ -2,6 +2,12 @@ package main // mainパッケージであることを宣言
 
 import "fmt" // fmtモジュールをインポート
 
-func main() { // 最初に実行されるmain()関数を定義
+//lint:ignore U1000 //
+/*
+func main() {
+	onea()
+}
+*/
+func onea() { // 最初に実行されるmain()関数を定義
 	fmt.Println("Hello World")
 }
