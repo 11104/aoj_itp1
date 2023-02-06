7465a7814d1dda917913329593e0101302779596
nh <nh@nhMac14.local>
2023-02-06 12:44:43 +0900

    add 1c

--- ITP1/1a.go
+++ ITP1/1a.go
@@ -1,12 +1,11 @@
 package main // mainパッケージであることを宣言
 
 import "fmt" // fmtモジュールをインポート
-
 //lint:ignore U1000 //
 /*
-func main() {
-	onea()
-}
+	func main() {
+		onea()
+	}
 */
 func onea() { // 最初に実行されるmain()関数を定義
 	fmt.Println("Hello World")
