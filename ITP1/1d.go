e2ba2ac6ba7fec6f4f8936715d6ea61d2a499cbb
nh <85434372+11104@users.noreply.github.com>
2023-02-06 13:03:12 +0900

    add 1d

---
+++ ITP1/1d.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+/*
+//
+//lint:ignore U1000 //
+
+	func main() {
+		oned()
+	}
+*/
+func oned() {
+	var d, h, m, s int
+	fmt.Scanf("%d", &d)
+	h = d / 3600
+	d %= 3600
+	m = d / 60
+	s = d % 60
+	fmt.Printf("%d:%d:%d\n", h, m, s)
+}
