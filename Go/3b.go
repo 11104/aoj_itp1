bc42bc9c6f87d681ead738957c4cebec5c8f6b44
nh <85434372+11104@users.noreply.github.com>
2023-02-07 11:48:30 +0900

    add 3b.go

---
+++ Go/3b.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+//lint:ignore U1000 //
+
+// /*
+func main() {
+	threeb()
+}
+
+//*/
+
+func threeb() {
+	a := []int{}
+	//スライス
+	count := 0
+	var tmp int
+	for i := 0; i < 10000; i++ {
+		//Golangにwhileは無い
+		fmt.Scanf("%d", &tmp)
+		if tmp == 0 {
+			break
+		}
+		count++
+		a = append(a, tmp)
+	}
+	for i := 0; i < count; i++ {
+		fmt.Printf("Case %d: %d\n", i+1, a[i])
+	}
+
+}
