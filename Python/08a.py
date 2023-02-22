e6d758bdac71e23136de12eb61e8b18b98358440
11104 <85434372+11104@users.noreply.github.com>
2023-02-22 10:15:11 +0900

    add 8a.py

---
+++ Python/08a.py
@@ -0,0 +1,18 @@
+#swapcase
+s=input()
+print(s.swapcase())
+
+#isOO?
+'''
+n=input()
+#For finish swap
+m=''
+for i in range(len(n)):
+    c=str(n[i])
+    if(n[i].islower()):
+        c=n[i].upper()
+    elif(n[i].isupper()):
+        c=n[i].lower()
+    m+=c
+print(m)
+'''
